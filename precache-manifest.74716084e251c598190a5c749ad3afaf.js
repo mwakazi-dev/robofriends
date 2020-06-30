4a095ea4e01d2a2063979f3e79aab10248be0da5
jmwakz99 <kemsjunior99@gmail.com>
2020-06-30 21:57:27 +0300

    Updates

---
+++ precache-manifest.74716084e251c598190a5c749ad3afaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ee0dee893299cbe0fafef259cc23af",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9d048f7c6be52c540aa3",
+    "url": "/my-app/static/css/main.8d48ba50.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/my-app/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d048f7c6be52c540aa3",
+    "url": "/my-app/static/js/main.cbaa7afe.chunk.js"
+  },
+  {
+    "revision": "c2a1dcc6438bccc67aae",
+    "url": "/my-app/static/js/runtime-main.813cd07b.js"
+  }
+]);
